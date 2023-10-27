397cf6102062aecc1ef19c753dd4a5ad54e6e8d2
etheoo <etheoo@protonmail.com>
2023-10-27 19:19:20 +0200

    Refactoring

---
+++ IGeometri.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labb7Polymorphism
+{ // Angelica Lindström NET.23
+    public interface IGeometri // parentClass
+    {
+        // Interfacets kontrakt säger att ärvda klasser måste implementera följande:
+        public string Name { get; }
+        public void Area();
+    }
+}
